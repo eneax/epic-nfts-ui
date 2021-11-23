1f8fd58fa99a620ca0d8c566e0c879cb33470912
eneax <19818937+eneax@users.noreply.github.com>
2021-11-23 21:03:46 +0100

    Ask contract to mint Nft

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
-export const OPENSEA_LINK = "";
+export const OPENSEA_LINK = "https://testnets.opensea.io";
 export const TOTAL_MINT_COUNT = 50;
+export const CONTRACT_ADDRESS = "0xA5fD85dBA6206A89017e2894bf0b6815cAA82e73";

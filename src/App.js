1edc769650a04a4db2ee5ae6b1f0639ede557c66
eneax <19818937+eneax@users.noreply.github.com>
2021-11-24 19:39:19 +0100

    Setup Event Listener

--- src/App.js
+++ src/App.js
@@ -28,6 +28,9 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account);
+
+      // Setup listener! In case a user ALREADY had their wallet connected + authorized
+      setupEventListener();
     } else {
       console.log("No authorized account found");
     }
@@ -50,6 +53,39 @@ const App = () => {
       // Print out public address once we authorize Metamask
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
+
+      // Setup listener! In case where a user connected their wallet for the first time
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Setup listener
+  const setupEventListener = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicNft.abi,
+          signer
+        );
+
+        // Capture event when contract throws it
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber());
+          alert(
+            `Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          );
+        });
+
+        console.log("Setup event listener!");
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
     } catch (error) {
       console.log(error);
     }
@@ -87,6 +123,10 @@ const App = () => {
     }
   };
 
+  React.useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   // Render Methods
   const renderNotConnectedContainer = () => (
     <button
@@ -97,9 +137,14 @@ const App = () => {
     </button>
   );
 
-  React.useEffect(() => {
-    checkIfWalletIsConnected();
-  }, []);
+  const renderMintUI = () => (
+    <button
+      onClick={askContractToMintNft}
+      className="cta-button connect-wallet-button"
+    >
+      Mint NFT
+    </button>
+  );
 
   return (
     <div className="App">
@@ -109,16 +154,9 @@ const App = () => {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {currentAccount === "" ? (
-            renderNotConnectedContainer()
-          ) : (
-            <button
-              onClick={askContractToMintNft}
-              className="cta-button connect-wallet-button"
-            >
-              Mint NFT
-            </button>
-          )}
+          {currentAccount === ""
+            ? renderNotConnectedContainer()
+            : renderMintUI()}
         </div>
         <div className="footer-container">
           <a
